626fa5fa647c381a906023d0cd70646de9983df1
Eliseu Nogueira <adm@eliseunogueira.com.br>
2025-04-29 12:03:24 -0300

    Build Expo, with some extra lints

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+	return (
+		<View>
+			<Text>Index</Text>
+		</View>
+	)
+}
+export default Index
+const styles = StyleSheet.create({})
